ff9893a11297aa3355245700a9243cb05f7775e1
Mikha <milenikaiqbal@gmail.com>
2025-03-28 07:08:28 +0700

    updated main.py to listen to 0.0.0.0

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 from backend.app.app import app  # Import FastAPI app from backend/app/app.py
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
